def81cee2a490e1f51ef3f66e825040f4dd469ed
Mateusz <mateusz.ostrozka34@gmail.com>
2023-12-31 12:58:08 +0100

    Twoja wiadomość zatwierdzenia

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+
+    const int rozmiarTablicy = 12;
+    int mojaTablica[rozmiarTablicy];
+
+    for (int i = 0; i < rozmiarTablicy; ++i) {
+        mojaTablica[i] = rand() % 100;  
+    }
+
+
+    cout << "Pseudolosowe liczby w tablicy:\n";
+    for (int i = 0; i < rozmiarTablicy; ++i) {
+        cout << mojaTablica[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
